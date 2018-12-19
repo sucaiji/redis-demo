5fe696a7b1f8a473fc21b82e177f2c08ba3d747d
sucaiji <sucaiji@aliyun.com>
2018-12-19 10:32:58 +0800

    修改一处jedis被误用的bug 将jedis放入jedis连接池 增加定时清理token的功能

--- cookie-cache/src/main/java/com/sucaiji/cookiecache/service/MyService.java
+++ cookie-cache/src/main/java/com/sucaiji/cookiecache/service/MyService.java
@@ -1,15 +1,23 @@
 package com.sucaiji.cookiecache.service;
 
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 import java.util.Date;
+import java.util.Set;
 import java.util.UUID;
 
 @Service
 public class MyService {
 
-    private Jedis jedis = new Jedis("localhost");
+    GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+
+    JedisPool jedisPool = new JedisPool(poolConfig, "127.0.0.1", 6379);
+
+
 
     /**
      * 检查登陆是否成功，account和password一样即可登录成功
@@ -20,7 +28,9 @@ public class MyService {
     public String checkLogin(String account, String password) {
         if (account.equals(password)) {
             String token = UUID.randomUUID().toString();
+            Jedis jedis = jedisPool.getResource();
             jedis.hset("login", token, account);
+            jedis.close();
             updateToken(token);
             return token;
         }
@@ -34,7 +44,10 @@ public class MyService {
      */
     public String checkToken(String token) {
         updateToken(token);
-        return jedis.hget("login", token);
+        Jedis jedis = jedisPool.getResource();
+        String account = jedis.hget("login", token);
+        jedis.close();
+        return account;
     }
 
     /**
@@ -43,7 +56,31 @@ public class MyService {
      */
     public void updateToken(String token) {
         long time = new Date().getTime();
+        Jedis jedis = jedisPool.getResource();
         jedis.zadd("recent", time, token);
+        jedis.close();
+    }
+
+    private static final long LIMIT = 100000L;
+
+    @Scheduled(cron = "1,10,20,30,40,50 * * * * ?")
+    public void cleanToken() {
+        try (Jedis jedis = jedisPool.getResource()){
+            long size = jedis.zcard("recent");
+            if (LIMIT >= size) {
+                System.out.println("当前token数为:" + size + "未超过，return");
+                return;
+            }
+            Set<String> tokens = jedis.zrange("recent", 0, size - LIMIT - 1);
+            System.out.println("size" + size);
+            System.out.println("tokensize" + tokens.size());
+            for (String token: tokens) {
+                jedis.zrem("recent", token);
+                jedis.hdel("login", token);
+            }
+            System.out.println("删除了" + (size - LIMIT) + "条数据,目前token数为:" +  jedis.zcard("recent"));
+        }
+
     }
 
 
--- cookie-cache/src/test/java/com/sucaiji/cookiecache/CookieCacheApplicationTests.java
+++ cookie-cache/src/test/java/com/sucaiji/cookiecache/CookieCacheApplicationTests.java
@@ -1,6 +1,7 @@
 package com.sucaiji.cookiecache;
 
 import com.sucaiji.cookiecache.controller.MyController;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 import java.net.URL;
 import java.net.URLConnection;
@@ -22,23 +25,27 @@ public class CookieCacheApplicationTests {
 
     @Test
     public void contextLoads() {
+        int count = 1000;
+        long sum = 0;
 
+        while (true) {
+            CountDownLatch cdl = new CountDownLatch(count);
 
-        int count = 10;
-        CountDownLatch cdl = new CountDownLatch(count);
+            for (int i = 0; i < count; i++) {
+                final long I = sum;
+                sum ++;
+                new Thread(() -> {
+                    cdl.countDown();
+                    System.out.println(restTemplate.getForObject("http://localhost:8080/login?account=" + I + "&password=" + I, String.class));
+                }).start();
+            }
+            try {
+                cdl.await();
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
-        for (int i = 0; i < count; i++) {
-            final int I = i;
-            new Thread(() -> {
-                cdl.countDown();
-                System.out.println(restTemplate.getForObject("http://localhost:8080/login?account=" + I + "&password=" + I, String.class));
-            }).start();
-        }
-        try {
-            cdl.await();
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
     }
 }
